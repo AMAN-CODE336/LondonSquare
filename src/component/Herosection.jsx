8f43de34cc47740ca343193357869df5dbf354fe
AMAN-CODE336 <amansaini25372gmail.com>
2025-06-11 12:44:19 +0530

    commit

--- src/component/Herosection.jsx
+++ src/component/Herosection.jsx
@@ -69,10 +69,10 @@ export default function Herosection() {
         </div> */}
               </Swiper>
               <div className=''>
-             <div className=" absolute bottom-16  md:bottom-[-50] left-[10rem] lg:left-[18.3rem] right transform -translate-x-1/2 z-20 bg-[#c26265] text-white py-4 lg:py-10 lg:space-y-4  rounded-md text-center lg:max-w-xl w-full shadow-lg">
+             <div className=" absolute bottom-16  md:bottom-[-50] left-[10rem] lg:left-[18.3rem] right transform -translate-x-1/2 z-20 bg-[#c26265] text-white py-4 lg:py-10 lg:space-y-4 pl-8  rounded-md md:text-center lg:max-w-xl w-full shadow-lg">
   <h2 className="text-xl md:text-5xl font-bold uppercase">Studio Apartments</h2>
   <h2 className="text-xl md:text-4xl uppercase ">Above Commercial Spaces</h2>
-  <p className="text-base md:text-xl px-6 md:px-0 tracking-wide uppercase">you're always close to everything you need</p>
+  <p className="text-base md:text-xl  md:px-0 tracking-wide uppercase">you're always close to everything you need</p>
 </div>
 </div>
 
