8e96d4b7d177c31202a2aa6a00093a3f1094e263
AMAN-CODE336 <amansaini25372gmail.com>
2025-06-14 13:02:30 +0530

    commit

--- src/component/ContactSection.jsx
+++ src/component/ContactSection.jsx
@@ -1,16 +1,62 @@
+"use client";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 export default function ContactSection() {
+  const formRef = useRef();
+  const [loading, setLoading] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true); // disable button immediately
+
+    emailjs
+      .sendForm(
+        "service_w6v7s4i",     
+        "template_8104w1j",    
+        formRef.current,
+        "E_SgzXoES6C29-ojJ"      
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message sent successfully!");
+          formRef.current.reset();
+          setLoading(false); // enable button again
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Something went wrong. Please try again.");
+          setLoading(false); // enable button again
+        }
+      );
+  };
+
   return (
-   <section id="Contactsection" className="w-full  px-6 py-12 md:mt-18 bg-white md:flex md:items-start md:justify-between gap-12">
+    <section
+      id="Contactsection"
+      className="w-full px-6 py-12 md:mt-18 bg-white md:flex md:items-start md:justify-between gap-12"
+    >
       {/* Left Side - Location Info */}
       <div className="md:w-1/2 w-full md:px-8 space-y-4">
-        <h2 className="text-3xl font-bold "><span className="text-[#c26265]">Visit</span> <span className="text-black">Our</span> <span className="text-[#c26265]">Office</span> </h2>
+        <h2 className="text-3xl font-bold ">
+          <span className="text-[#c26265]">Visit</span>{" "}
+          <span className="text-black">Our</span>{" "}
+          <span className="text-[#c26265]">Office</span>{" "}
+        </h2>
         <p className="text-gray-600">
-We’re more than happy to connect with you! Whether you have a question, a project idea, or just want to say hello — our doors are always open. <br/> 
-Feel free to drop by our office and meet us in person. Prefer writing to us? Simply fill out the form alongside and share your query.<br/> 
-One of our team members will get back to you promptly. Let's start the conversation!        </p>
+          We’re more than happy to connect with you! Whether you have a
+          question, a project idea, or just want to say hello — our doors are
+          always open. <br />
+          Feel free to drop by our office and meet us in person. Prefer writing
+          to us? Simply fill out the form alongside and share your query.
+          <br />
+          One of our team members will get back to you promptly. Let's start the
+          conversation!
+        </p>
         <iframe
           title="Google Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3434.879844723186!2d76.70805697557442!3d30.58093717465788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fe9489f3bf8ef%3A0x5668ce646a11aae7!2sMDB%20LONDON%20SQUARE!5e0!3m2!1sen!2sin!4v1749109809989!5m2!1sen!2sin" 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3434.879844723186!2d76.70805697557442!3d30.58093717465788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fe9489f3bf8ef%3A0x5668ce646a11aae7!2sMDB%20LONDON%20SQUARE!5e0!3m2!1sen!2sin!4v1749109809989!5m2!1sen!2sin"
           className="w-full h-64 mt-12 rounded-md border border-gray-200"
           allowFullScreen=""
           loading="lazy"
@@ -18,8 +64,14 @@ One of our team members will get back to you promptly. Let's start the conversat
       </div>
 
       {/* Right Side - Enquiry Form */}
-      <form className="md:w-1/2 w-full mt-10 md:mt-0 space-y-6">
-        <h3 className="text-2xl font-semibold text-gray-800 mb-5">Got Questions? We've Got Answers</h3>
+      <form
+        ref={formRef}
+        onSubmit={sendEmail}
+        className="md:w-1/2 w-full mt-10 md:mt-0 space-y-6"
+      >
+        <h3 className="text-2xl font-semibold text-gray-800 mb-5">
+          Got Questions? We've Got Answers
+        </h3>
 
         {[
           { label: "Full Name", id: "name", type: "text" },
@@ -30,17 +82,18 @@ One of our team members will get back to you promptly. Let's start the conversat
           <div key={id} className="relative">
             <input
               id={id}
+              name={id}
               type={type}
               placeholder=" "
               required
-                autoComplete="off"
+              autoComplete="off"
               className="peer block px-4 w-full border-b-2 border-gray-300 bg-transparent py-5 px-0 text-gray-900 placeholder-transparent focus:outline-none focus:border-[#c26265]"
             />
             <label
               htmlFor={id}
-className="absolute px-2 mt-1 left-0 top-[-5]  text-sm transition-all 
-  peer-placeholder-shown:top-4 peer-placeholder-shown:text-base 
-  peer-focus:top-[-5] peer-focus:text-sm peer-focus:text-[#c26265]"
+              className="absolute px-2 mt-1 left-0 top-[-5] text-sm transition-all 
+                peer-placeholder-shown:top-4 peer-placeholder-shown:text-base 
+                peer-focus:top-[-5] peer-focus:text-sm peer-focus:text-[#c26265]"
             >
               {label}
             </label>
@@ -51,25 +104,30 @@ className="absolute px-2 mt-1 left-0 top-[-5]  text-sm transition-all
         <div className="relative">
           <textarea
             id="message"
+            name="message"
             placeholder=" "
             rows="4"
             required
-              className="peer block px-4 w-full border-b-2 border-gray-300 bg-transparent py-5 px-0 text-gray-900 placeholder-transparent focus:outline-none focus:border-[#c26265]"
+            className="peer block px-4 w-full border-b-2 border-gray-300 bg-transparent py-5 px-0 text-gray-900 placeholder-transparent focus:outline-none focus:border-[#c26265]"
           ></textarea>
           <label
             htmlFor="message"
-className="absolute px-2 mt-1 left-0 top-[-5]  text-sm transition-all 
-  peer-placeholder-shown:top-4 peer-placeholder-shown:text-base 
-  peer-focus:top-[-5] peer-focus:text-sm peer-focus:text-[#c26265]"          >
-           Tell us what’s on your mind...
+            className="absolute px-2 mt-1 left-0 top-[-5] text-sm transition-all 
+              peer-placeholder-shown:top-4 peer-placeholder-shown:text-base 
+              peer-focus:top-[-5] peer-focus:text-sm peer-focus:text-[#c26265]"
+          >
+            Tell us what’s on your mind...
           </label>
         </div>
 
         <button
           type="submit"
-          className="inline-block mt-4 bg-[#c26265] text-white font-semibold px-6 py-3 rounded-full  transition duration-300"
+          disabled={loading}
+          className={`inline-block mt-4 bg-[#c26265] text-white font-semibold px-6 py-3 rounded-full transition duration-300 ${
+            loading ? "opacity-60 cursor-not-allowed" : ""
+          }`}
         >
-          Submit Enquiry
+          {loading ? "Sending..." : "Submit Enquiry"}
         </button>
       </form>
     </section>
